56645c039f0e9adeee7892bab8193bfb97e7b5c8
xupengpeng <xupengpeng@daojia-inc.com>
2021-10-18 17:20:03 +0800

    第一次提交

---
+++ app/src/main/java/com/payphone/sunnyweather/App.kt
@@ -0,0 +1,13 @@
+package com.payphone.sunnyweather
+
+import android.app.Application
+
+/**
+ * @author: Payphone7
+ * @date: 2021/10/18
+ */
+class App : Application() {
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
